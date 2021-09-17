759d201f46f6b625b3fc5030fe65e48bbcbe3475
Thomas <fearherbs1@gmail.com>
2021-09-17 13:29:45 -0400

    Add files via upload

---
+++ week3/dnsscanner.ps1
@@ -0,0 +1,6 @@
+﻿$network=$args[0]
+$dnsserver=$args[1]
+for (($i =1); ($i -lt 256); ($i++)){
+    $address=$network+"."+$i
+    Resolve-DnsName -DnsOnly $address -Server $dnsserver -ErrorAction Ignore
+}
